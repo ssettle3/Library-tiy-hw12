0309380ea18897627e4d9ec54650836c3e536517
Stephen Settle <ssettle3@gmail.com>
2015-02-02 22:39:05 -0500

    gulp

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,57 @@
+var gt = {};
+
+// Lists all coaches since 1980
+gt.coaches = [
+	{ 	name: 'Bill Curry',
+		years: '1980, 1981, 1982, 1983, 1984, 1985, 1986',
+		wins: 31,
+		losses: 43
+	},
+	{	name: 'Bobby Ross',
+		years: '1987, 1988, 1989, 1990, 1991',
+		wins: 31,
+		losses: 26
+	},
+	{	name: 'Bill Lewis',
+		years: '1992, 1993, 1994',
+		wins: 11,
+		losses: 22
+	}, 
+	{	name: 'George OLeary',
+		years: '1995, 1996, 1997, 1998, 1999, 2000, 2001',
+		wins: 53,
+		losses: 30
+	},
+	{	name: 'Chan Gailey',
+		years: '2002, 2003, 2004, 2005, 2006, 2007',
+		wins: 44,
+		losses: 33
+	},
+	{	name: 'Paul Johnson',
+		years: '2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015',
+		wins: 58,
+		losses: 35
+	},
+]
+
+// Adding Numbers
+gt.add = function (a, b){
+	return (a + b);
+}
+
+// Coaches that have more than 40 wins 
+
+	gt.wins = gt.coaches.filter(function (coach){
+		if (coach.wins > 40){
+			return coach.name;
+		} 
+	});
+	gt.winWin = gt.wins.map( function (coach){
+		if(coach.wins > 40)
+		return coach.name;
+		console.log( 'These coaches have/had over 40 wins')
+	});
+	
 stephen = {}
 
 
